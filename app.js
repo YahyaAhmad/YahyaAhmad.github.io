077f202469f4cdb9b03b3110e957526d7c4a4fa5
yahya omar ali ahmad <mugewara@hotmail.com>
2018-08-23 21:46:18 +0300

    fixed 404 error on get

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 var app = angular.module("pageApp",["ngAnimate"]);
 
 
-app.controller("test",function($scope){
-
+app.controller("test",function($scope,$interval){
+$scope.stop = undefined;
 $scope.htmlFile = 'templates/home.html';
 
+$scope.setInclude = function(file){
+    $scope.htmlFile = file;
+}
+
 $scope.changeInclude = function(file){
+    $scope.errorF = '';
     $scope.classLoader = '';
     $scope.htmlFile = `templates/${file}`;
     
 }
 
 $scope.$on("$includeContentLoaded",function(event,templete){
-
+    
     $scope.classLoader = 'opacity_hide';
+    if(angular.isDefined($scope.stop)){
+    console.log("The stop was defined, stop now is undefined.");
+    $interval.cancel($scope.stop);
+    $scope.stop = undefined;
+    }
+
 
 });
+
+$scope.$on("$includeContentError",function(event,templete){
+
+    if(angular.isDefined($scope.stop)) return;
+    $scope.stop = $interval(function(){$scope.htmlFile = templete + '?update=' + new Date().getTime();},1000);
+    $scope.errorF = 'error';
+});
+
+
     
 });
