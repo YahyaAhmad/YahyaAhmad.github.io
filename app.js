4f72fd7c555862c3b1e1b0edfd8b773afc05183d
yahya omar ali ahmad <mugewara@hotmail.com>
2018-08-23 18:39:30 +0300

    Added a css loader

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var app = angular.module("pageApp",["ngAnimate"]);
 app.controller("test",function($scope){
 
 $scope.htmlFile = 'templates/home.html';
-
+$('.loader').addClass('opacity_hide');
 $scope.changeInclude = function(file){
-
+    $('.loader').removeClass('opacity_hide');
     $scope.htmlFile = `templates/${file}`;
-
+    $('.loader').addClass('opacity_hide');
 }
     
 });
