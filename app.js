49633b1da8f700b849b4a2f3ebf8503993ff2a5d
yahya omar ali ahmad <mugewara@hotmail.com>
2018-08-24 17:38:31 +0300

    Added popup

--- app.js
+++ app.js
@@ -1,9 +1,67 @@
 var app = angular.module("pageApp",["ngAnimate"]);
 
+app.directive('dirMessage',function(){
 
-app.controller("test",function($scope,$interval){
+    return{
+
+        templateUrl:'templates/modal.html'
+
+    }
+
+});
+
+app.service('modalService',function(){
+
+    this.makeMessage = function(type){
+
+        var messageObject = {};
+
+        if (type=='contact'){
+            
+            messageObject = {
+            header: "CONTACT ME",
+            body: "<strong>yahyaahmad@gmail.com</strong>"
+           };
+        }
+
+        if (type=='cv'){
+            
+            messageObject = {
+            header: "MY CV",
+            body: "<a href='yahya_ahmad_cv.docx'>DOWNLOAD</a>"
+           };
+        }
+
+        return messageObject;
+
+    }
+
+});
+
+
+
+app.controller("test",function($scope,$interval,$timeout,modalService,$sce){
 $scope.stop = undefined;
-$scope.htmlFile = 'templates/home.html';
+$scope.exitClickable = true;
+$scope.showPopup = false;
+$scope.trust = $sce.trustAsHtml;
+
+$timeout(function(){ $scope.htmlFile = 'templates/home.html'; },500);
+
+$scope.changeShow = function(type){ 
+
+    if(type==''){
+        if(!$scope.exitClickable) return;
+        $scope.exitClickable = false;
+        $scope.showPopup = false;
+        return;
+    }
+    $scope.exitClickable = true;
+    var messageObject = modalService.makeMessage(type);
+    $scope.header = messageObject.header;
+    $scope.body = messageObject.body;
+    $scope.showPopup = !$scope.showPopup;
+ }
 
 $scope.setInclude = function(file){
     $scope.htmlFile = file;
